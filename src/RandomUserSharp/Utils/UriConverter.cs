e17f01d9d715ec6730515d399e549bf1d5044319
Sebastian Jensen <apps@tsjdev-apps.de>
2023-09-07 17:57:21 +0200

    Add custom UriConverter for Newtonsoft.Json.

---
+++ src/RandomUserSharp/Utils/UriConverter.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace RandomUserSharp.Utils
+{
+    internal class UriConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Uri);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return Uri.TryCreate(reader.Value.ToString(), UriKind.Absolute, out var uri) ? uri : null;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
